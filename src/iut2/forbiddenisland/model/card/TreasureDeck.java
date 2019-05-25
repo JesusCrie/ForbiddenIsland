e7d0fecf2dcdd2d6190bd2b7678b9f488ac2f49a
Lucas <lucas.malandrino@gmail.com>
2019-05-25 12:34:38 +0200

    Generalized decks

--- src/iut2/forbiddenisland/model/card/TreasureDeck.java
+++ src/iut2/forbiddenisland/model/card/TreasureDeck.java
@@ -1,32 +1,9 @@
 package iut2.forbiddenisland.model.card;
 
-import java.util.*;
+public class TreasureDeck extends Deck<TreasureCard> {
 
-public class TreasureDeck implements Deck {
-
-	private final List<TreasureCard> originalCards = new ArrayList<>();
-	private final LinkedList<TreasureCard> deck = new LinkedList<>();
-	private final LinkedList<TreasureCard> discardingDeck = new LinkedList<>();
-
-	@Override
-	public Card drawCard() {
-		return deck.pop();
-	}
-
-	@Override
-	public void discardCard(Card f) {
-		discardingDeck.addLast((TreasureCard) f);
-	}
-
-
-
-	@Override
-	public void reset() {
-
-	}
-
-	@Override
-	public void shuffle() {
-		Collections.shuffle(deck);
-	}
+    public TreasureDeck() {
+        // TODO register all treasure cards
+        super(null);
+    }
 }
