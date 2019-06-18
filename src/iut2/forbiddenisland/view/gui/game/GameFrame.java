67d262c13eafd9108c2aa6505565583fc465ee68
joris <joris.chenevas@free.fr>
2019-06-18 14:57:59 +0200

    DiscardFrame and  discard method (GameFrame)

--- src/iut2/forbiddenisland/view/gui/game/GameFrame.java
+++ src/iut2/forbiddenisland/view/gui/game/GameFrame.java
@@ -1,9 +1,13 @@
 package iut2.forbiddenisland.view.gui.game;
 
+import iut2.forbiddenisland.model.card.TreasureCard;
 import iut2.forbiddenisland.view.gui.utils.ConstraintFactory;
 
 import javax.swing.*;
-import java.awt.GridBagLayout;
+import java.awt.*;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class GameFrame extends JFrame {
 
@@ -29,5 +33,18 @@ public class GameFrame extends JFrame {
                 ConstraintFactory.fillBoth(0, 3, 3, 1));
         add(new ActionPanel(this),
                 ConstraintFactory.fillBoth(3, 3, 1, 1));
+    }
+
+    public TreasureCard discardCard(List<TreasureCard> list) {
+        final CompletableFuture<TreasureCard> future = new CompletableFuture<>();
+
+        final DiscardFrame frame = new DiscardFrame(list, future);
+        frame.setVisible(true);
+
+        try {
+            return future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new IllegalStateException("Future failed", e);
+        }
     }
 }
