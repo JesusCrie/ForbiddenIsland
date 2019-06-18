b663de61415de9b7045b2f333c2693a44d6f5bba
Lucas Malandrino <lucas.malandrino@gmail.com>
2019-06-18 15:31:59 +0200

    Fully committed AutoResizePreserveRatioImagePanel

--- src/iut2/forbiddenisland/view/gui/utils/AutoResizePreserveRatioImagePanel.java
+++ src/iut2/forbiddenisland/view/gui/utils/AutoResizePreserveRatioImagePanel.java
@@ -1,6 +1,38 @@
 package iut2.forbiddenisland.view.gui.utils;
 
 import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.Image;
 
-public class AutoResizePreserveProportionImagePanel extends JPanel {
+public class AutoResizePreserveRatioImagePanel extends JPanel {
+
+    private final Image image;
+    private final boolean isHorizontal;
+    private final double aspectRatio;
+
+    public AutoResizePreserveRatioImagePanel(final Image image) {
+        this.image = image;
+        isHorizontal = image.getWidth(null) > image.getHeight(null);
+        aspectRatio = (double) image.getWidth(null) / image.getHeight(null);
+    }
+
+    @Override
+    protected void paintComponent(final Graphics g) {
+        int x = 0, y = 0;
+        double width, height;
+
+
+        if (isHorizontal) {
+            y = (getHeight() - image.getHeight(null)) / 2;
+            width = getWidth();
+            height = width * aspectRatio;
+
+        } else {
+            x = (getWidth() - image.getWidth(null)) / 2;
+            height = getHeight();
+            width = height / aspectRatio;
+        }
+
+        g.drawImage(image, x, y, (int) width, (int) height, null);
+    }
 }
