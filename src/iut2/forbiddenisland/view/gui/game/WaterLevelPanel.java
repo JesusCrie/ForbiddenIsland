f80f505a5bae6526d668874543ce4067d03a5af9
joris <joris.chenevas@free.fr>
2019-06-18 16:16:59 +0200

    WaterLevelPanel draft

--- src/iut2/forbiddenisland/view/gui/game/WaterLevelPanel.java
+++ src/iut2/forbiddenisland/view/gui/game/WaterLevelPanel.java
@@ -1,12 +1,27 @@
 package iut2.forbiddenisland.view.gui.game;
 
-import javax.swing.*;
-import java.awt.Color;
-import java.awt.Component;
+import iut2.forbiddenisland.view.gui.utils.AutoResizePreserveRatioImagePanel;
 
-public class WaterLevelPanel extends JPanel {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class WaterLevelPanel extends JLayeredPane {
+
 
     public WaterLevelPanel(final Component parent) {
-        setBackground(Color.BLUE);
+        AutoResizePreserveRatioImagePanel fond;
+        try {
+             fond = new AutoResizePreserveRatioImagePanel(
+                    ImageIO.read(getClass().getResourceAsStream("/Niveau.png"))
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+            fond = null;
+        }
+        fond.setSize(300,200);
+        add(fond, 1);
     }
 }
