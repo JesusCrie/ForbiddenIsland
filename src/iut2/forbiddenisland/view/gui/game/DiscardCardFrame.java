c8611fa184e72b0db3ae505ca9cf099922228e7c
joris <joris.chenevas@free.fr>
2019-06-19 14:57:25 +0200

    Repair of WaterLevelPanel

--- src/iut2/forbiddenisland/view/gui/game/DiscardCardFrame.java
+++ src/iut2/forbiddenisland/view/gui/game/DiscardCardFrame.java
@@ -9,7 +9,7 @@ import java.awt.*;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public class DiscardCardFrame extends JFrame {
+public class DiscardCardFrame extends JDialog {
 
     public DiscardCardFrame(final List<TreasureCard> treasureCards, final CompletableFuture<TreasureCard> future) {
 
@@ -17,23 +17,36 @@ public class DiscardCardFrame extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         setAlwaysOnTop(true);
+        setModal(true);
 
         setLayout(new GridBagLayout());
         GridBagConstraints c;
 
         final JLabel label = new JLabel("Choisissez une carte à jeter", JLabel.CENTER);
-        c = ConstraintFactory.fillHorizontal(0, 0, treasureCards.size(), 1);
+        c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
         c.insets = new Insets(0, 0, 15, 0);
+        c.gridx = 0;
+        c. gridy = 0;
+        c.gridwidth = treasureCards.size();
         add(label, c);
 
+        c.insets = new Insets(0,5,0,5);
+        c.ipadx = 75;
+        c.ipady = 150;
+        c.gridwidth = 1;
+        c.gridy = 1;
         for (int i = 0; i < treasureCards.size() - 1; i++) {
             final TreasureCard card = treasureCards.get(i);
             final CardButton button = createCardButton(card, new Insets(0, 5, 0, 5), future, i);
+            c.gridx = i;
             add(button, c);
         }
 
         final TreasureCard card = treasureCards.get(treasureCards.size() - 1);
         final JButton button = createCardButton(card, new Insets(0, 35, 0, 0), future, treasureCards.size() - 1);
+        c.gridx = treasureCards.size() - 1;
+        c.insets = new Insets(0,35,0,5);
         add(button, c);
     }
 
