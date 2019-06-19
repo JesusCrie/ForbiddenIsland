ea86550f208e3b1528e1ba403eb67c6eda5626b4
joris <joris.chenevas@free.fr>
2019-06-19 15:04:40 +0200

    Repair of WaterLevelPanel.

--- src/iut2/forbiddenisland/view/gui/game/DiscardCardFrame.java
+++ src/iut2/forbiddenisland/view/gui/game/DiscardCardFrame.java
@@ -13,7 +13,7 @@ public class DiscardCardFrame extends JDialog {
 
     public DiscardCardFrame(final List<TreasureCard> treasureCards, final CompletableFuture<TreasureCard> future) {
 
-        setSize(100 * treasureCards.size() + 185 , 250);
+        setSize(100 * treasureCards.size() + 185, 250);
         setResizable(false);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         setAlwaysOnTop(true);
@@ -27,38 +27,33 @@ public class DiscardCardFrame extends JDialog {
         c.fill = GridBagConstraints.HORIZONTAL;
         c.insets = new Insets(0, 0, 15, 0);
         c.gridx = 0;
-        c. gridy = 0;
+        c.gridy = 0;
         c.gridwidth = treasureCards.size();
         add(label, c);
 
-        c.insets = new Insets(0,5,0,5);
+        c.insets = new Insets(0, 5, 0, 5);
         c.ipadx = 75;
         c.ipady = 150;
         c.gridwidth = 1;
         c.gridy = 1;
+
         for (int i = 0; i < treasureCards.size() - 1; i++) {
             final TreasureCard card = treasureCards.get(i);
-            final CardButton button = createCardButton(card, new Insets(0, 5, 0, 5), future, i);
+            final CardButton button = createCardButton(card, future);
             c.gridx = i;
             add(button, c);
         }
 
         final TreasureCard card = treasureCards.get(treasureCards.size() - 1);
-        final JButton button = createCardButton(card, new Insets(0, 35, 0, 0), future, treasureCards.size() - 1);
+        final JButton button = createCardButton(card, future);
         c.gridx = treasureCards.size() - 1;
-        c.insets = new Insets(0,35,0,5);
+        c.insets = new Insets(0, 35, 0, 5);
         add(button, c);
     }
 
-    private CardButton createCardButton(final TreasureCard card, final Insets insets,
-                                        final CompletableFuture<TreasureCard> future, final int index) {
+    private CardButton createCardButton(final TreasureCard card, final CompletableFuture<TreasureCard> future) {
 
         final CardButton btn = new CardButton(card);
-
-        final GridBagConstraints cts = ConstraintFactory.create(index, 1);
-        cts.ipadx = 75;
-        cts.ipady = 150;
-        cts.insets = insets;
 
         btn.addActionListener(e -> {
             dispose();
