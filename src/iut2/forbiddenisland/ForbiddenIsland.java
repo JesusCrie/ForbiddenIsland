78b2786f046aa0f40862d4c526ac9c639408b5b1
Lucas Malandrino <lucas.malandrino@gmail.com>
2019-06-19 08:48:19 +0200

    Created dummy controller

--- src/iut2/forbiddenisland/ForbiddenIsland.java
+++ src/iut2/forbiddenisland/ForbiddenIsland.java
@@ -1,32 +1,18 @@
 package iut2.forbiddenisland;
 
 import iut2.forbiddenisland.controller.Controller;
-import iut2.forbiddenisland.model.Board;
-import iut2.forbiddenisland.model.adventurer.*;
-import iut2.forbiddenisland.view.DemoBoard;
+import iut2.forbiddenisland.controller.DummyController;
 import iut2.forbiddenisland.view.gui.game.GameFrame;
 
 import javax.swing.*;
-import java.util.Arrays;
-import java.util.List;
 
 public class ForbiddenIsland {
 
     public static void main(String[] args) {
+        final Controller controller = new DummyController();
+
+        // Delegate to swing thread
         SwingUtilities.invokeLater(() -> {
-            final List<Adventurer> adventurers = Arrays.asList(
-                    new Pilot("Jean Mich"),
-                    new Navigator("Jacquie"),
-                    new Explorer("Jeane"),
-                    new Messenger("Pierrot"),
-                    new Diver("Philippe !"),
-                    new Engineer("Gertrude")
-            );
-
-            final Board board = DemoBoard.createAndGet(adventurers);
-
-            final Controller controller = new Controller(board, adventurers);
-
             final GameFrame frame = new GameFrame(controller);
             frame.setVisible(true);
         });
---
+++ src/iut2/forbiddenisland/controller/DummyController.java
@@ -0,0 +1,24 @@
+package iut2.forbiddenisland.controller;
+
+import iut2.forbiddenisland.model.Board;
+import iut2.forbiddenisland.model.adventurer.*;
+import iut2.forbiddenisland.view.DemoBoard;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DummyController extends Controller {
+
+    private static List<Adventurer> DUMMY_ADVENTURERS = Arrays.asList(
+            new Pilot("Gégé"),
+            new Diver("Pedro"),
+            new Engineer("Mc Gyver"),
+            new Messenger("Batman")
+    );
+
+    private static Board DUMMY_BOARD = DemoBoard.createAndGet(DUMMY_ADVENTURERS);
+
+    public DummyController() {
+        super(DUMMY_BOARD, DUMMY_ADVENTURERS);
+    }
+}
